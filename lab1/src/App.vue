899d8c60caf06935db41e177ed85e0a74bf6e275
Zhansulu Shakhizada <zhansulushakhizada@gmail.com>
2024-09-10 22:18:27 +0500

    finished lab 1

---
+++ lab1/src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+    <header>
+      <div class="left-part">
+        Mini Blog
+      </div>
+      <nav class="right-part">
+        <a href="#">Home</a>
+        <a href="#">Politics</a>
+        <a href="#">Tech</a>
+        <a href="#">Entertainment</a>
+        <a href="#">Travel</a>
+        <a href="#">Sports</a>
+        <button>
+          <img src="https://img.icons8.com/?size=256&id=112468&format=png" alt="Icon">
+        </button>
+      </nav>
+    </header>
+    <main class="grid">
+      <BlogList></BlogList>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+*{
+  font-family: Bahnschrift;
+}
+
+.container{
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f3f2f2;
+}
+
+header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px 30px;
+  font-size: 20px;
+  background-color: white;
+}
+
+.left-part {
+  font-size: 30px;
+  font-weight: bold;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  width: 40%;
+  justify-content: space-between;
+}
+
+img {
+  width: 40px;
+}
+
+button {
+  background-color: transparent;
+  border: none;
+  border-radius: 50%;
+  transition: all 400ms ease;
+}
+
+button:hover {
+  background-color: rgba(0,0,0,0.1);
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+a::after {
+  content: '';
+  display: block;
+  width: 0;
+  height: 3px;
+  background: #818080;
+  transition: width 0.3s;
+}
+
+a:hover::after {
+  width: 100%;
+}
+
+main{
+    padding: 50px 0 50px 0;
+    justify-content: center;
+    align-items: center;
+
+}
+</style>
+<script setup lang="ts">
+import BlogList from "@/components/BlogList.vue";
+</script>
---
+++ lab1/src/components/BlogList.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="bloglist-container">
+    <div v-for="(blog, index) in blogList" :key="index" class="bloglist-item" :class="'item-' + (index + 1)">
+      <div class="image-container">
+        <img :src="blog.img" />
+        <div class="overlay">
+          <h2>{{ blog.title }}</h2>
+          <p id="publication_date">{{ blog.publDate }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bloglist-container {
+  display: grid;
+  grid-template-columns: repeat(3, 400px);
+  grid-template-rows: repeat(2, auto);
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+}
+
+.bloglist-item {
+  color: #f3f2f2;
+  width: 400px;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+img {
+  width: 100%;
+}
+
+h2 {
+  font-size: 20px;
+}
+
+p {
+  font-size: 18px;
+  opacity: 0.8;
+}
+
+.image-container {
+  position: relative;
+}
+
+.overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 10px;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%);
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.item-1, .item-2 {
+  grid-column: 1;
+}
+.item-1, .item-3, .item-4{
+  grid-row: 1;
+}
+.item-2, .item-5{
+  grid-row: 2;
+}
+.item-3 {
+  grid-column: 2;
+  grid-row: span 2;
+  img{
+    height: 480px;
+  }
+}
+
+.item-4, .item-5 {
+  grid-column: 3;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      blogList: [
+        { title: 'Trump’s claim terrorists are pouring over southern border does not stand up to scrutiny', publDate: 'Sun September 8, 2024', img: 'https://media.cnn.com/api/v1/images/stellar/prod/c-gettyimages-2167172665.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp'},
+        { title: '2024 NFL season preview: a potential historic three-peat, rookie quarterbacks and first foray into South America', publDate: 'Thu September 5, 2024', img: 'https://media.cnn.com/api/v1/images/stellar/prod/240211225757-73-super-bowl-58.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp'},
+        { title: 'This hurricane season is confounding experts and defying forecasts. What the heck is going on?', publDate: 'Fri September 6, 2024', img: 'https://media-cdn.tripadvisor.com/media/photo-s/1b/69/39/6e/lakhta-center.jpg'},
+        { title: 'Everything you need to know about prepping your home for hurricane season', publDate: 'Wed August 21, 2024', img: 'https://media.cnn.com/api/v1/images/stellar/prod/lead-1.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp'},
+        { title: 'The fall forecast is in and there’s some bad news for fall lovers', publDate: 'Wed August 28, 2024', img: 'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1244301968.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp'}
+      ]
+    };
+  }
+};
+</script>
